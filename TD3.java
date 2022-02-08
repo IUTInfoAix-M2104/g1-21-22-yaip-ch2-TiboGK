a97e8489ee36dcf6164c974804abf13e91ae41ca
Thibaut LAHAIE <91830243+TiboGK@users.noreply.github.com>
2022-02-08 17:22:41 +0100

    Add files via upload

--- TD3.java
+++ TD3.java
@@ -89,6 +89,104 @@ public class TD3 {  // Save as "TD3.java"
 			System.out.println(inStr + " is NOT a binary string");
 		} 
 	}
+	
+	public static void Exo89(){
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter a hex string : ");
+		String inStr = in.nextLine();
+		in.close();
+	     
+		int inStrLen = inStr.length();
+		char inChar;
+		boolean isValid ; 
+		
+		isValid = true ; 
+		for (int i=0 ; i < inStrLen ; i++ ) { 
+			inChar = inStr.charAt(i); 
+			if (!((inChar >= '0' && inChar <= '9')|| (inChar >= 'A' && inChar <= 'F')|| (inChar >= 'a' && inChar <= 'f'))) { 
+				isValid = false; 
+				break;  
+			} 
+		} 
+		if (isValid) { 
+			System.out.println(inStr + " is a hex string"); 
+		} else { 
+			System.out.println(inStr + " is NOT a hex string");
+		} 
+	}
+	
+	public static void Exo93(){
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the number of students : ");
+		int number = in.nextInt();
+		
+		int[] students;
+		students = new int[number];
+		for (int i=0 ; i < number ; i++ ) { 
+			System.out.print("Enter the grade for students "+ (i+1) + " : ");
+			students[i]= in.nextInt();
+		}
+		in.close();
+		
+		int average = 0;
+		
+		for (int j=0 ; j < number ; j++ ) { 
+			average+=students[j];
+		}
+		
+		int min = students[0];
+		int max = students[0];
+		
+		for (int k=1 ; k < number ; k++ ){ 
+			if (students[k] < min){
+				min = students[k];
+			}
+			if (students[k] > max){
+				max = students[k];
+			}
+		}
+		
+		average=average/number;
+		System.out.println("Average is "+average);
+		System.out.println("Max is "+ max);
+		System.out.println("Min is "+ min);	
+	}
+	
+	public static void Exo101(){
+		int exp; 
+		int base; 
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the base : ");
+		base = in.nextInt();
+		
+		System.out.print("Enter the exp : ");
+		exp = in.nextInt();
+		System.out.println(base + " raises to the power of " + exp + " is: " + exponent(base, exp));
+	}
+
+	public static int exponent(int base, int exp) {
+		int product = 1;
+
+		for (int i=0 ; i < exp ; i++ ) {
+			product *= base;
+		}
+
+		return product;
+	}
+	
+	public static void Exo102(){
+		int exp;    // exponent (non-negative integer)
+		int base;   // base (integer)
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the base : ");
+		base = in.nextInt();
+		
+		System.out.print("Enter the exp : ");
+		exp = in.nextInt();
+		System.out.println(base + " raises to the power of " + exp + " is: " + exponent(base, exp));
+	}
    
    
    public static void main(String[] args) {
@@ -122,10 +220,46 @@ public class TD3 {  // Save as "TD3.java"
 		*/
 		
 	//Test avec plusieurs valeurs de l'exo 8.8
-		
+		/*
 		System.out.println( "Exo 8.8" );
 		Exo88();
 		System.out.println();
+		*/
+		
+	//Test avec plusieurs valeurs de l'exo 8.9
+		/*
+		System.out.println( "Exo 8.9" );
+		Exo89();
+		System.out.println();
+		*/
+		
+	//Test avec plusieurs valeurs de l'exo 9.1
+		/*
+		System.out.println( "Exo 9.1" );
+		Exo91();
+		System.out.println();
+		*/
+		
+	//Test avec plusieurs valeurs de l'exo 9.3
+		/*
+		System.out.println( "Exo 9.3" );
+		Exo93();
+		System.out.println();
+		*/
+		
+	//Test avec plusieurs valeurs de l'exo 10.1
+		/*
+		System.out.println( "Exo 10.1" );
+		Exo101();
+		System.out.println();
+		*/
+		
+	//Test avec plusieurs valeurs de l'exo 10.2
+		
+		System.out.println( "Exo 10.2" );
+		Exo102();
+		System.out.println();
+		
 		
 		
 	}
